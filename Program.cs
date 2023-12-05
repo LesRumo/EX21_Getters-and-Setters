87d239a52ba4ad054883aa0fd1326b278d9d8408
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-05 16:04:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX21_Getters_and_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movies1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movies2 = new Movie("Unreleased", "Evans", "PG");
+
+            Console.WriteLine(movies1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
